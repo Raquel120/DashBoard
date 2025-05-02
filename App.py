13e5dfd8d33cb59c5d13bc50680fcba10b82f653
Raquel120 <raquelconceicao@sapo.pt>
2025-05-02 21:11:36 +0100

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,4 @@
-git init
-git remote add origin https://github.com/Raquel120/dashboard-dash-app.git
-git add .
-git commit -m "Primeiro commit"
-git branch -M main
-git push -u origin main
 
+git add requirements.txt
+git commit -m "Adicionado requirements.txt"
+git push origin main
